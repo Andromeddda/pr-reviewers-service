199c5223c3e5a64df69c3b2395cb4b53d17862e9
Andromeddda <andrey.shik2005@gmail.com>
2025-11-19 23:09:01 +0300

    update go.sum

--- prs/cmd/prs/main.go
+++ prs/cmd/prs/main.go
@@ -1,5 +1,20 @@
 package main
 
+import (
+	_ "encoding/json"
+	_ "fmt"
+	_ "log"
+	_ "math/rand"
+	_ "net/http"
+	_ "os"
+	_ "strings"
+	_ "time"
+
+	_ "github.com/go-chi/chi/v5"
+	_ "gorm.io/driver/postgres"
+	_ "gorm.io/gorm"
+)
+
 func main() {
 	
 }
