ee36b8f78df84732623b3b500f0ff8485a3dc03d
Andromeddda <andrey.shik2005@gmail.com>
2025-11-20 15:03:39 +0300

    add postgres config and dsn from env

---
+++ prs/internal/config/config.go
@@ -0,0 +1,48 @@
+package config
+
+import (
+	"fmt"
+	"os"
+)
+
+type PostgresConfig struct {
+	Host string
+	Port string 
+	User string 
+	Pass string
+	DB 	 string 
+}
+
+type Config struct {
+	Postgres PostgresConfig
+}
+
+func LoadConfig() (*Config) {
+	cfg := &Config{
+		Postgres: PostgresConfig{
+			Host: getEnvOrDefault("POSTGRES_HOST", "localhost"),
+			Port: getEnvOrDefault("POSTGRES_PORT", "5432"),
+			User: getEnvOrDefault("POSTGRES_USER", "postgres"),
+			Pass: getEnvOrDefault("POSTGRES_PASS", "postgres"),
+			DB:   getEnvOrDefault("POSTGRES_DB",   "prs"),
+		},
+	}
+
+	return cfg
+}
+
+
+func DSN(cfg PostgresConfig) string {
+	return fmt.Sprintf(
+        "host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
+        cfg.Host, cfg.User, cfg.Pass, cfg.DB, cfg.Port,
+    )
+}
+
+func getEnvOrDefault(key, def string) string {
+	v := os.Getenv(key)
+	if v != "" {
+		return v 
+	}
+	return def
+}
