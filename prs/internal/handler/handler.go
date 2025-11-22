7ba1f124abc573849f080dbbfd0bf10016bc7cf2
Andromeddda <andrey.shik2005@gmail.com>
2025-11-22 19:07:42 +0300

    fix HTTP error response

--- prs/internal/handler/handler.go
+++ prs/internal/handler/handler.go
@@ -3,6 +3,7 @@ package handler
 import (
 	"encoding/json"
 	"errors"
+	"fmt"
 	"log"
 	"net/http"
 	"prs/internal/dto"
@@ -20,6 +21,20 @@ func NewPRSHandler(s service.PRService) *PRSHandler {
 	}
 }
 
+func (h* PRSHandler) writeError(w http.ResponseWriter, status int, code dto.ErrorCode, message string) {
+	w.Header().Set("Content-Type", "application/json")
+    w.WriteHeader(status)
+
+    resp := dto.ErrorResponse{
+        Error: dto.ErrorObj{
+            Code:    code,
+            Message: message,
+        },
+    }
+
+    json.NewEncoder(w).Encode(resp)
+}
+
 func (h* PRSHandler) AddTeam(w http.ResponseWriter, r *http.Request) {
 	var request dto.Team
 
@@ -33,8 +48,9 @@ func (h* PRSHandler) AddTeam(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		// 400
 		if errors.Is(err, repository.ErrTeamExist) {
-			log.Printf("ERROR: Team already exist: %s", err.Error())
-			http.Error(w, err.Error(), http.StatusBadRequest)
+			message := fmt.Sprintf("AddTeam team_name=%s : %s", request.TeamName, err.Error())
+			log.Println(message)
+			h.writeError(w, http.StatusBadRequest, dto.ErrorTeamExist, message)
 			return
 		}
 
@@ -63,8 +79,9 @@ func (h* PRSHandler) GetTeam(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		// 404
 		if errors.Is(err, repository.ErrTeamNotFound) {
-			log.Printf("ERROR: Team not found: %s", err.Error())
-			http.Error(w, err.Error(), http.StatusNotFound)
+			message := fmt.Sprintf("GetTeam team_name=%s : %s", team_name, err.Error())
+			log.Println(message)
+			h.writeError(w, http.StatusNotFound, dto.ErrorNotFound, message)
 			return
 		}
 
