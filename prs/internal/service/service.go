94af08e830f4381cdf6412aa944cba51c0555516
Andromeddda <andrey.shik2005@gmail.com>
2025-11-22 14:29:42 +0300

    service.AddTeam() template

--- prs/internal/service/service.go
+++ prs/internal/service/service.go
@@ -1,13 +1,36 @@
 package service
 
+import (
+	"context"
+	"prs/internal/dto"
+	"prs/internal/repository"
+)
+
 type PRService interface {
+	AddTeam(ctx context.Context, team *dto.Team) (*dto.Team, *dto.ErrorResponse)
+ 
+	// TODO: GetTeam
+	// TODO: UserSetIsActive
+	// TODO: CreatePullRequest
+	// TODO: MergePullRequest
+	// TODO: ReassignPullRequest
+	// TODO: UserGetReview
 
 }
 
 type prservice struct {
-
+	repo 	repository.Repository
 }
 
-func NewPRService() PRService {
-	return &prservice{}
+func NewPRService(repository repository.Repository) PRService {
+	return &prservice{
+		repo: repository,
+	}	
 }
+
+func (prs *prservice) AddTeam(ctx context.Context, team *dto.Team) (*dto.Team, *dto.ErrorResponse) {
+	// TODO
+	return team, nil
+}
+
+
